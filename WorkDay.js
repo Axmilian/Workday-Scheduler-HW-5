4b1d049f8b89f0bc3bade80a8ee19c7006ab4fa5
Harrison Noblett <HarrisonNoblett55@gmail.com>
2020-11-14 18:22:22 -0600

    first commit with js not saving to local storage

---
+++ WorkDay.js
@@ -0,0 +1,27 @@
+//Display Current Time-----------------
+var day = moment().format("dddd MMMM Do, YYYY");
+$("#currentDay").text(day);
+var now = moment().format('LT');
+$("#currentTime").text(now)
+
+var colorTime = moment().hour();
+
+$(".description").each(function colorCode() {
+
+    var milTime = $(this).attr("id");
+
+    if (colorTime == milTime) {
+        $(this).addClass("present");
+    }else if (colorTime > milTime) {
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+});
+
+$(".saveBtn").each(function saveLocal() {
+    
+})
